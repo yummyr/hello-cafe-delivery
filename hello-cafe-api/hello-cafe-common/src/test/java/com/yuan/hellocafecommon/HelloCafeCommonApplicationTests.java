17e9c71f7a4f31b88ee9927235ba72551dcd1bdb
yummyr <ttang1873@gmail.com>
2025-10-14 00:04:31 -0700

    Fix git structure and add server module

---
+++ hello-cafe-api/hello-cafe-common/src/test/java/com/yuan/hellocafecommon/HelloCafeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.hellocafecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCafeCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
